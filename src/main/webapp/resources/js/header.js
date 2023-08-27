bfe7979d5cc94bf63a1a61b40d1d455a08c62276
qoa1193 <qoa1093@gmail.com>
2023-08-27 20:20:06 +0900

    헤더

--- src/main/webapp/resources/js/header.js
+++ src/main/webapp/resources/js/header.js
@@ -1,12 +1,13 @@
-$(document).ready(function(e){    
-//e.prevetnDefault();    
+$(document).ready(function(){      
 var menubar = "<nav class='menuBarOpen'><ul>"
 			 +"<li class='menubarblock'><a href='#'>마이페이지</a></li>"
 			 +"<li class='menubarblock'>한줄리뷰게시판</li>"
 			 +"<li class='menubarblock'>이미지게시판</li></ul></nav>";
  $('#menu').append(menubar);
  $(".menuBarOpen").hide();
- $('#menu').on("click", function() {
+ $("#menu").on("click", function(e) {
+ 
+ 		e.stopPropagation();
         $(".menuBarOpen").slideToggle();
 
 	})
@@ -21,26 +22,3 @@ $(document).on("click", function (e) {
        $(".menuBarOpen").slideUp();
    }
 });
-
-/*$(document).ready(function(){        
-var menubar = "<nav class='menuBarOpen'><ul>"
-			 +"<li class='menubarblock'><a href='#'>마이페이지</a></li>"
-			 +"<li class='menubarblock'>한줄리뷰게시판</li>"
-			 +"<li class='menubarblock'>이미지게시판</li></ul></nav>";
- $('#menu').append(menubar);
-    $(".menuBarOpen").hide();
-   $('#menu').on("click", function() {
-        $(".menuBarOpen").slideToggle();
-
-	})
-	$('.menubarblock').on("click",function(e){
-		e.stopPropagation();
-	
-	}) 
-})
-$(document).on("click", function (e) {
-   // 토글 영역이 아닌 영역을 클릭하면 토글을 숨김
-   if (!$(e.target).closest('.menuBarOpen').length) {
-       $(".menuBarOpen").slideUp();
-   }
-});*/
